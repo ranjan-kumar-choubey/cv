76f3f5fd4c0eb48525b653d5797033fac385b9f9
Ranjan Kumar Choubey <ranjankumarchoubey999@gmail.com>
2023-10-04 17:09:33 +0530

    lab-test solution added

--- code/notebook/test.c
+++ code/notebook/test.c
@@ -1,65 +1,27 @@
-/* Q. Write a Program which will take two command line agument
-	first argument : line No. of a file
-	second argument : File Name
-Example : ./test.out <line_number> <input.txt>
+#include<stdio.h>
 
-*/
+#define NUM_BINS 10
 
-#include<stdio.h> 
-# include<stdlib.h>
-void main(int argc, char **argv)
+int hist[NUM_BINS];
+
+int main(int ac, char *av[])
 {
-    // printf("argc = %d\n",argc);
+   int x;
+	// intialize array with zero
+	for (int i = 0 ;i<NUM_BINS;i++)
+		hist[i] = 0;
+	
+	// Take input one by one and count 
+    while (1 == scanf("%d", &x)) {
+		int index = x/100;
+		if (index>=0 && index < NUM_BINS)
+			hist[index] ++;
+       
+    }
 
-	// 	print everything get by command line
-    // for(int i=0;i<argc; i++)
-    //     printf("argv[%d] = %s\n",i,argv[i]);
+	// print Histogram
+	for(int i = 0;i <NUM_BINS; i++)
+		printf(" %d",hist[i]);
 
-
-	int n = atoi(argv[1]); //string to integer
-	if(n<=0)
-	{
-		printf("Line No must be positive\n");
-		exit(1);
-	}
-
-	// float f = atof("98981.299999"); //string to float
-	// printf("float f = %f\n",f);
-
-	// check you have provided correct no of argument
-	if(argc != 3)
-        printf("Usage %s <line number> <fileneame> \n",argv[0]);
-
-	// check you are successfully read or not ?
-	FILE * fp = fopen(argv[2],"r");
-	if (NULL == fp)
-		printf("\n could not open the file %s\n",argv[2]);
-
-	//  reading charcter by character & throw 
-	int current_line = 1;
-	char c;
-
-	while(current_line < n)
-	{
-		// reading file line by line & dicarding 
-		do c = fgetc(fp);  
-		while(c != EOF && c != '\n');
-
-		// if less no of lines in file than given value 
-		if(c == EOF)
-		{
-			printf("Not Enough lines \n"); 
-			exit(1);
-		}
-		current_line++;
-	}
-
-	// now pointer is pointing to that line , & print that line 
-	 while(EOF != (c=fgetc(fp)))
-	 {
-		putchar(c);
-		if(c=='\n')
-			break;
-	 }
-
+    return 0;
 }
