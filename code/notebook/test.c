28e169ef2ca4c12b86e21c7cb1928dcfad3dd035
Ranjan Kumar Choubey <ranjankumarchoubey999@gmail.com>
2023-10-09 16:08:19 +0530

    class 2

--- code/notebook/test.c
+++ code/notebook/test.c
@@ -1,27 +1,7 @@
 #include<stdio.h>
-
-#define NUM_BINS 10
-
-int hist[NUM_BINS];
-
-int main(int ac, char *av[])
-{
-   int x;
-	// intialize array with zero
-	for (int i = 0 ;i<NUM_BINS;i++)
-		hist[i] = 0;
-	
-	// Take input one by one and count 
-    while (1 == scanf("%d", &x)) {
-		int index = x/100;
-		if (index>=0 && index < NUM_BINS)
-			hist[index] ++;
-       
-    }
-
-	// print Histogram
-	for(int i = 0;i <NUM_BINS; i++)
-		printf(" %d",hist[i]);
-
+#include<string.h>
+int main() {
+    char *str="ranjan";
+    printf("Size of the string = %lu", strlen(str)); 
     return 0;
 }
